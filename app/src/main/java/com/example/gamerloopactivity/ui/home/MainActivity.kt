a9f6003c75c1890ea08a3b13d42dd11733b51c3d
MarcproDev <cartageno888@gmail.com>
2025-10-20 15:25:08 -0300

    bottom nav view nav controller items

--- app/src/main/java/com/example/gamerloopactivity/ui/home/MainActivity.kt
+++ app/src/main/java/com/example/gamerloopactivity/ui/home/MainActivity.kt
@@ -3,17 +3,34 @@ package com.example.gamerloopactivity.ui.home
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.gamerloopactivity.R
 import com.example.gamerloopactivity.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initUI()
+
+    }
+
+    private fun initUI() {
+        intNavigation()
+    }
+
+    private fun intNavigation() {
+        val navHost = supportFragmentManager.findFragmentById(R.id.activityContainerView) as NavHostFragment
+        navController = navHost.navController
+        binding.bottomNavView.setupWithNavController(navController)
 
     }
 }
